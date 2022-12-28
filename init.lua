c5f939998ec5564b4b48b62a8fc131bb4fd9c4cc
unknown <24964441+wsor4035@users.noreply.github.com>
2022-12-28 00:20:34 -0500

    add drawer

--- init.lua
+++ init.lua
@@ -7,3 +7,14 @@ minetest.register_node("interior_decor:crate", {
     wield_scale = vector.new(0.0625,0.0625,0.0625),
     groups = {oddly_breakable_by_hand = 3},
 })
+
+minetest.register_node("interior_decor:drawer", {
+    description = "interior decor drawer",
+    drawtype = "mesh",
+    paramtype2 = "facedir",
+    mesh = "interior_decor_drawer.obj",
+    tiles = {"interior_decor_drawer.png"},
+    visual_scale = 0.0625, -- 1/16
+    wield_scale = vector.new(0.0625,0.0625,0.0625),
+    groups = {oddly_breakable_by_hand = 3},
+})

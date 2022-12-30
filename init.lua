08c679876bf956967d75376775236204d2cf52f4
unknown <24964441+wsor4035@users.noreply.github.com>
2022-12-30 00:56:14 -0500

    add storage space to the crate and make it save on dig and show up on place

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
---most models just need to be moved +/-8 to get them centered in blender
-
 minetest.register_node("interior_decor:crate", {
     description = "interior decor crate",
     drawtype = "mesh",
@@ -8,7 +6,57 @@ minetest.register_node("interior_decor:crate", {
     visual_scale = 0.0625, -- 1/16
     wield_scale = vector.new(0.0625,0.0625,0.0625),
     groups = {oddly_breakable_by_hand = 3},
-    --TODO: add storage or something
+    on_construct = function(pos)
+        minetest.get_meta(pos):get_inventory():set_size("main", 9*3)
+    end,
+    on_place = function(itemstack, placer, pointed_thing)
+        local ipv = minetest.item_place(itemstack, placer, pointed_thing)
+
+        if itemstack:get_meta():get("contents") then
+            local contents = minetest.deserialize(itemstack:get_meta():get("contents"))
+            for k,v in pairs(contents) do
+                contents[k] = ItemStack(v)
+            end
+
+            minetest.get_meta(pointed_thing.above):get_inventory():set_list("main", contents)
+        end
+        if itemstack:get_meta():get("description") then
+            minetest.get_meta(pointed_thing.above):set_string(
+                "description", itemstack:get_meta():get("description")
+            )
+        end
+        return ipv
+    end,
+    on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+        local fnpos = table.concat({pos.x, pos.y, pos.z}, ",")
+        local fs = {
+            "size[8,8]",
+            "style_type[list;spacing=0.1]",
+            "list[nodemeta:" .. fnpos .. ";main;0,0.3;9,3;]",
+            "style_type[list;spacing=0.25]", --reset to default
+            "list[current_player;main;0,3.75;8,4;]",
+            "listring[nodemeta:" .. fnpos .. ";main]",
+            "listring[current_player;main]",
+        }
+        --TODO: support i3, show description if present in the formspec - else default to playername crate
+
+        minetest.show_formspec(clicker:get_player_name(), "interior_decor:crate_fs", table.concat(fs, ""))
+    end,
+    preserve_metadata = function(pos, oldnode, oldmeta, drops)
+        local meta = minetest.get_meta(pos)
+
+        local stringlist = {}
+        for k,v in pairs(meta:get_inventory():get_list("main")) do
+            stringlist[#stringlist+1] = v:to_string()
+        end
+        --minetest meta limits will be an issue way be minetest.serialize failing will be
+        drops[1]:get_meta():set_string("contents", minetest.serialize(stringlist))
+
+        if meta:get("description") then
+            drops[1]:get_meta():set_string("description", minetest.get_meta(pos):get_string("description"))
+        end
+    end,
+
 })
 
 minetest.register_node("interior_decor:drawer", {
